8a79969f649bff169360999d0e47bc4ef7ba31fa
ramallahos <110548111+ramallahos@users.noreply.github.com>
2022-09-19 23:01:25 +0000

    Delete launcher.sh

--- launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-stylenum1=75
-stylenum2=16
-num1="$(expr $(( $RANDOM % $stylenum1 )) + 1 )"
-num2="$(expr $(( $RANDOM % $stylenum2 )) + 1 )"
-echo $num1 $num2
-theme="style-$num1"
-sed -i -e "s/@import .*/@import \"$num2.rasi\"/g" $theme.rasi || true
-rofi -show drun -theme ${theme}.rasi
